78f37f6f7fa9d577ba37fcff172c341caabf1c97
Niraj1996abcd <sanjaynks1996@gmail.com>
2023-09-27 20:13:06 +0530

    Sanjay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Blog from "./components/Blog";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Service from "./components/Service";
+import Skills from "./components/Skills";
+import Testmonial from "./components/Testmonial";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <About />
+      <Service />
+      <Portfolio />
+      <Skills />
+      <Testmonial />
+      <Blog />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
